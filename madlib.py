46b6068e1fc9ea5c6b53a47c544adf5f50f1628e
tylermatrix <tylermaatrix@gmail.com>
2020-08-14 18:39:42 -0400

    Create madlib.py
    simple input into concat program

---
+++ madlib.py
@@ -0,0 +1,7 @@
+colour = input("Enter a colour: ")
+plural_noun = input("Enter a pluraized noun: ")
+celebrity = input("Enter your favourite celeb: ")
+
+print("Roses are " + colour)
+print(plural_noun + " are blue")
+print("I love" + celebrity)
